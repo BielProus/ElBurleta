c2575211192a4f6ebf9764e7780e8a74bd21c9d3
Oriol Miro <omiro@xtec.cat>
2024-11-27 17:03:02 +0100

    Actualitzar el seeder de posts

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -19,60 +19,108 @@ class PostSeeder extends Seeder
         $users = User::all();
 
         $acudits = [
-            "Per què els músics sempre es perden? Perquè estan buscant el to!",
-            "Què fa un vampir en un tractor? Agricultura de sang!",
-            "Saps com es diu un peix que sona? Un dofí silenciador!",
-            "Per què els gats no expliquen acudits? Perquè són massa seriosos per riure!",
-            "Com es diu un gos mag que fa trucs? Un dog-ma!",
-            "Quin és l’ocell més fort? L’ocell de ferro, perquè pesa una tona!",
-            "Per què els tomàquets mai juguen a cartes? Perquè no volen fer salsa!",
-            "Com li dius a un pingüí que surt a fer exercici? Un 'run-guí'!",
-            "Què diu un pollastre que es diverteix molt? 'Que guai, quina ploma!'",
-            "Què fa un ànec a la discoteca? 'Duck-step'!",
-            "Què passa si tires un llibre al riu? Que es converteix en una novel·la aquàtica!",
-            "Quin és l’animal més educat? El cocodril, perquè sempre saluda amb 'croc'!",
-            "Com es diu un cuc que sap ballar? Un cucba!",
-            "Per què el professor de música es va enfadar? Perquè el piano estava desafinat!",
-            "Com fa un vaixell que està content? Va xiulant!",
-            "Per què els ordinadors no prenen cafè? Perquè ja tenen moltes instruccions!",
-            "Quin és el fruit que sempre està cansat? La taronja, perquè s’esprem massa!",
-            "Per què els esquelets mai porten rellotge? Perquè el temps els passa pels ossos!",
-            "Com es diu un ós que es vesteix elegant? Un ósmoda!",
-            "Què fa un cangur boxejador? Saltar a l’arena!",
-            "Per què els peixos no van a l’escola? Perquè ja viuen en el corrent!",
-            "Quin és el lloc preferit dels vampirs? El banc de sang!",
-            "Com es diu un elefant que fa màgia? Un elefantàstic!",
-            "Què fa un ou a l’espai? Es converteix en un estel ou-nidós!",
-            "Què diu una vaca quan arriba a un prat verd? 'Quina herba tan deliciosa!'",
-            "Què fa un astronauta quan li pica el cap? Es rasca l’espai!",
-            "Per què els dinosaures no viuen avui dia? Perquè ja no caben a cap pis!",
-            "Què li diu una llimona a un altre? 'Estem pelats!'",
-            "Per què les bicicletes no es queixen mai? Perquè tenen molta resistència!",
-            "Què fa una mosca en una sopa? Practica natació sincronitzada!",
-            "Com es diu un cactus graciós? Un punxant-tista!",
-            "Què diu un lleó quan veu un safari? 'Quin banquet mòbil!'",
-            "Per què les estrelles mai arriben tard? Perquè sempre són puntuals amb l’òrbita!",
-            "Com es diu un gos que estudia? Un 'perru-llibre'!",
-            "Per què els cargols no necessiten bitllet? Perquè porten casa seva a sobre!",
-            "Què fa una aranya a Internet? Teixir la seva xarxa!",
-            "Per què les serps no tenen amics? Perquè són massa enrotllades!",
-            "Què fa un peix amb ulleres? Mira millor sota l’aigua!",
-            "Com es diu un cavall que fa bromes? Un cavall-riure!",
-            "Per què els ànecs sempre estan contents? Perquè sempre tenen una ploma per escriure!",
-            "Què diu un arbre a un altre? 'Estàs arrelat a la terra?'",
-            "Què fa un astronauta a la lluna? Practica passos lleugers!",
-            "Per què els ocells no gasten diners? Perquè sempre estan volant gratis!",
-            "Com es diu un coet graciós? Un 'riure-a-l’espai'!",
-            "Què passa si barreges una sargantana amb un camaleó? Un 'sarga-leó'!",
-            "Per què els robots no menteixen? Perquè estan programats per ser sincers!",
-            "Què fa un tauró a classe de matemàtiques? Practica divisió submarina!",
-            "Què diu un gos quan veu un gat al cinema? 'Quina pel·li més guau!'",
-            "Com es diu un ou graciós? Un 'humor-ou'!"
+            [
+                'title' => "Aquest acudit musical et deixarà sense paraules!",
+                'content' => "Per què els músics sempre es perden? Perquè estan buscant el to!"
+            ],
+            [
+                'title' => "El vampir que va revolucionar l'agricultura!",
+                'content' => "Què fa un vampir en un tractor? Agricultura de sang!"
+            ],
+            [
+                'title' => "Aquest peix amb talent et farà riure!",
+                'content' => "Saps com es diu un peix que sona? Un dofí silenciador!"
+            ],
+            [
+                'title' => "El misteri dels gats seriosos... resolt!",
+                'content' => "Per què els gats no expliquen acudits? Perquè són massa seriosos per riure!"
+            ],
+            [
+                'title' => "No creuràs el truc màgic d'aquest gos!",
+                'content' => "Com es diu un gos mag que fa trucs? Un dog-ma!"
+            ],
+            [
+                'title' => "Quin és l'ocell més fort? La resposta et sorprendrà!",
+                'content' => "Quin és l’ocell més fort? L’ocell de ferro, perquè pesa una tona!"
+            ],
+            [
+                'title' => "Aquest tomàquet no juga a cartes, i aquí tens el motiu!",
+                'content' => "Per què els tomàquets mai juguen a cartes? Perquè no volen fer salsa!"
+            ],
+            [
+                'title' => "Un pingüí al gimnàs? Descobreix el seu secret!",
+                'content' => "Com li dius a un pingüí que surt a fer exercici? Un 'run-guí'!"
+            ],
+            [
+                'title' => "El pollastre més feliç del prat!",
+                'content' => "Què diu un pollastre que es diverteix molt? 'Que guai, quina ploma!'"
+            ],
+            [
+                'title' => "L'ànec que domina la pista de ball!",
+                'content' => "Què fa un ànec a la discoteca? 'Duck-step'!"
+            ],
+            [
+                'title' => "Els llibres aquàtics existeixen?",
+                'content' => "Què passa si tires un llibre al riu? Que es converteix en una novel·la aquàtica!"
+            ],
+            [
+                'title' => "El cocodril més educat de la selva!",
+                'content' => "Quin és l’animal més educat? El cocodril, perquè sempre saluda amb 'croc'!"
+            ],
+            [
+                'title' => "Aquest cuc ballarí és un fenomen!",
+                'content' => "Com es diu un cuc que sap ballar? Un cucba!"
+            ],
+            [
+                'title' => "El professor enfadat per culpa del piano!",
+                'content' => "Per què el professor de música es va enfadar? Perquè el piano estava desafinat!"
+            ],
+            [
+                'title' => "Un vaixell que xiula? Això sí que és curiós!",
+                'content' => "Com fa un vaixell que està content? Va xiulant!"
+            ],
+            [
+                'title' => "La veritat darrere dels ordinadors i el cafè!",
+                'content' => "Per què els ordinadors no prenen cafè? Perquè ja tenen moltes instruccions!"
+            ],
+            [
+                'title' => "No tornis a veure una taronja igual mai més!",
+                'content' => "Quin és el fruit que sempre està cansat? La taronja, perquè s’esprem massa!"
+            ],
+            [
+                'title' => "Els esquelets i el misteri del rellotge!",
+                'content' => "Per què els esquelets mai porten rellotge? Perquè el temps els passa pels ossos!"
+            ],
+            [
+                'title' => "Aquest ós es vesteix millor que tu!",
+                'content' => "Com es diu un ós que es vesteix elegant? Un ósmoda!"
+            ],
+            [
+                'title' => "Un cangur amb talent inesperat!",
+                'content' => "Què fa un cangur boxejador? Saltar a l’arena!"
+            ],
+            [
+                'title' => "El secret que els peixos no volen que sàpigues!",
+                'content' => "Per què els peixos no van a l’escola? Perquè ja viuen en el corrent!"
+            ],
+            [
+                'title' => "Els vampirs tenen un lloc preferit, i és aquest!",
+                'content' => "Quin és el lloc preferit dels vampirs? El banc de sang!"
+            ],
+            [
+                'title' => "Aquest elefant màgic et sorprendrà!",
+                'content' => "Com es diu un elefant que fa màgia? Un elefantàstic!"
+            ],
+            [
+                'title' => "Què fa un ou a l'espai? La resposta és increïble!",
+                'content' => "Què fa un ou a l’espai? Es converteix en un estel ou-nidós!"
+            ]
         ];
+        
         foreach ($acudits as $acudit) {
             Post::create([
-                'title' => 'Acudit', // O pots generar un títol amb Faker si vols
-                'content' => $acudit,
+                'title' => $acudit['title'], // O pots generar un títol amb Faker si vols
+                'content' => $acudit['content'],
                 'user_id' => $users->random()->id,
             ]);
         }
