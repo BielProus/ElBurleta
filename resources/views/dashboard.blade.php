cfe158f72a118171faa43f567692621013b7d281
Johan <Johan@Joan.com>
2024-12-04 19:29:49 +0100

    UPGRADE

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,27 +1,59 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Burleta</title>
+    <style>
+        h1 {
+            margin-bottom: 20vh;
+            font-size: 15vh;
+            margin-top: 0px;
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            
+        }
 
-                <!-- Mostrar opciones administrativas solo para el usuario con ID 12 -->
-                @if(Auth::check() && Auth::id() == 12)
-                <a href="{{ route('posts.create') }}" class="btn btn-primary">Crear Post</a>
-                <a href="{{ route('posts.edit', $post->id ?? 0) }}" class="btn btn-secondary">Editar Post</a>
-                <form action="{{ route('posts.destroy', $post->id ?? 0) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Eliminar Post</button>
-                </form>
-                @endif
 
-            </div>
-        </div>
+        .container {
+            padding: 3% 5% 5%;
+            margin: 5% 20% 10%;
+            border-radius: 20px;
+            background-color: rgba(255,255,255,0.5);
+            display: block;
+            text-align: center;
+        }
+        body {
+            font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+            background-image: url("cocker.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            
+        }
+        .button {
+            margin: 10vh 0;
+            background-color: red;
+            padding: 5px;
+            border-radius: 15px;
+            text-decoration: none;
+            color: white;
+        }
+        .text {
+            margin-bottom: 20vh;
+        }
+    </style>
+</head>
+<body>
+        
+
+    <div class="container">
+        
+        <h1>El Burleta</h1>
+        <h2 class="text">Per què tan seriós?</h2>
+        <a class="button" href="https://www.burleta.cat">Comença a riure</a>
+        
+
     </div>
-</x-app-layout>
+    
+    <footer></footer>
+</body>
+</html>
